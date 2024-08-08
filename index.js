19d9553b10a7ccd6fd8fe17203feba25a5f38951
Jacob Ahrens <jahrens1003@gmail.com>
2024-08-08 11:44:20 -0400

    Update boilerplate with solution

--- index.js
+++ index.js
@@ -20,8 +20,29 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let dateString = req.params.date;
+
+  if(!dateString) {
+    const currDate = new Date();
+    res.json({
+      unix: currDate.getTime(),
+      utc: currDate.toUTCString()
+    });
+  } else {
+    if(!isNaN(dateString)) {
+      dateString = parseInt(dateString);
+    }
+    const date = new Date(dateString);
+    if (date.toString() === 'Invalid Date') {
+      res.json({ error: 'Invalid Date' });
+    } else {
+      res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString()
+      });
+    }
+  }
 });
 
 
